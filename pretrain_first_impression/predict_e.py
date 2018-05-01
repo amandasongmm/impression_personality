6c038a2466f659b3c797fd69b935decd5645d840
amandasongmm <feijuejuanling@gmail.com>
2018-05-01 12:08:21 -0700

    impression prediction update

--- pretrain_first_impression/predict_e.py
+++ pretrain_first_impression/predict_e.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.multioutput import MultiOutputRegressor
+from shutil import copyfile
 
 
 default_device = '/gpu:0'
@@ -203,7 +204,9 @@ def extract_features(image_directory, filenames, model_save_dir, batch_size=64):
 
 
 def extract_e_feature():
+    print('Start extracting features from various layers in VGG in E-dataset...')
     e_dir = '/home/amanda/Documents/cropped_face/e_with_mask'
+    model_save_dir = '/home/amanda/Documents/vgg_model/e_with_mask_feat'
 
     im_name_lst = [f for f in os.listdir(e_dir) if os.path.isfile(os.path.join(e_dir, f))]
     id_lst = [f[1:-7] for f in im_name_lst]
@@ -211,31 +214,32 @@ def extract_e_feature():
 
     e_im_lst = ['e' + im_id + '_cb.jpg' if 'e' + im_id + '_cb.jpg' in im_name_lst else 'e' + im_id + '_tw.jpg'
                 for im_id in unique_id_lst]
-    extract_features(image_directory=e_dir, filenames=e_im_lst,
-                     model_save_dir='/home/amanda/Documents/vgg_model/e_with_mask_feat')
+    extract_features(image_directory=e_dir, filenames=e_im_lst, model_save_dir=model_save_dir)
 
 
 def extract_2kface_feature():
-    img_dir = '/home/amanda/Downloads/2kfaces'
+    # img_dir = '/home/amanda/Downloads/2kfaces'  # Amanda office PC
+    img_dir = '/home/amanda/Github/attractiveness_datamining/MIT2kFaceDataset/2kfaces'  # Amanda home pc
     model_save_dir = '/home/amanda/Documents/vgg_model/2k_feat'
 
-    extract_features(image_directory=img_dir, filenames=os.listdir(img_dir), model_save_dir=model_save_dir)
+    file_names = [f for f in os.listdir(img_dir) if f[-4:] == '.jpg']  # make sure every file is readable.
+    extract_features(image_directory=img_dir, filenames=file_names, model_save_dir=model_save_dir)
 
 
-def split_on_2k(feat_layer_file_name='conv51.npy'):
-
+def split_on_2k(feat_layer_file_name='maxpool5.npy'):
+    start_t = time.time()
+    print('Start spliting the 2k dataset into training and testing sets...\n')
     model_save_dir = '/home/amanda/Documents/vgg_model/2k_feat'
     feat_layer_name = feat_layer_file_name[:-4]
     train_test_split_data_save_name = os.path.join(model_save_dir, feat_layer_name+'_train_test_data.npz')
 
     # load valid img lst
     valid_img_file_name = 'valid_img_lst.npy'
-    valid_img_lst = np.load(os.path.join(model_save_dir, 'valid_img_lst.npy'))  #
+    valid_img_lst = np.load(os.path.join(model_save_dir, valid_img_file_name))  #
 
     # load extracted feature from model_save_dir. Feature extraction layers include conv51,2,3, maxpool5, fc6, fc7.
-    sample_file_name = os.path.join(model_save_dir, feat_layer_file_name)
-    feat_arr = np.load(sample_file_name)  # 2176 * 100352, for example.
-    feat_num = feat_arr.shape[1]
+    feature_file_name = os.path.join(model_save_dir, feat_layer_file_name)
+    feat_arr = np.load(feature_file_name)  # 2176 * 100352, for example.
 
     # load selected impression attribute scores
     select_rating_path = 'tmp_data/selected_score.pkl'
@@ -251,56 +255,153 @@ def split_on_2k(feat_layer_file_name='conv51.npy'):
     # split all data into training and testing set.
     x_train, x_test, y_train, y_test = train_test_split(feat_arr, rating_arr, test_size=0.1, random_state=42)
     np.savez(train_test_split_data_save_name, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    print('Done! Elapsed time = {}'.format(time.time()-start_t))
+    return
 
-    # # apply PCA on training data.
-    # pca = PCA(n_components=0.95)
-    # pca.fit(x_train)
-    #
-    # # save the pca model
-    # pca_save_name = model_save_dir + '/' + feat_layer_name + '_keep_ratio_095_' + 'pca_model.sav'
-    # pickle.dump(pca, open(pca_save_name, 'wb'))
-    #
-    # # when loading the model: loaded_model = pickle.load(open(file_name), 'wb')
-    #
-    # # apply pca on raw feature matrix
-    # x_train_pca = pca.transform(x_train)
-    # x_test_pca = pca.transform(x_test)
-    #
-    # # multi_regressor = MultiOutputRegressor(Ridge())
-    # multi_regressor = MultiOutputRegressor(LinearRegression())
-    # multi_regressor.fit(x_train_pca, y_train)
-    #
-    # # save the regressor model
-    # regressor_save_name = model_save_dir + '/' + feat_layer_name + '_regressor_model.sav'
-    # pickle.dump(multi_regressor, open(regressor_save_name, 'wb'))
-    #
-    # # predict on training data
-    # y_train_predict = multi_regressor.predict(x_train_pca)
-    #
-    # # predict on test data
-    # y_test_predict = multi_regressor.predict(x_test_pca)
-    #
-    # # compare performance
-    # rating_names = ['attractive', 'aggressive', 'trustworthy', 'intelligent', 'sociable', 'responsible']
-    # for ind, cur_name in enumerate(rating_names):
-    #     print('Cur impression: {}'.format(cur_name))
-    #     train_gt = y_train[:, ind]
-    #     train_pred = y_train_predict[:, ind]
-    #     [train_cor, p] = pearsonr(train_gt, train_pred)
-    #
-    #     test_gt = y_test[:, ind]
-    #     test_pred = y_test_predict[:, ind]
-    #     [test_cor, p] = pearsonr(test_gt, test_pred)
-    #
-    #     print('train cor {}, test cor = {}\n'.format(train_cor, test_cor))
+
+def pca_and_regression_on_2k(feat_layer_file_name='conv53.npy', pca_keep_dim=150):
+
+    print('\nStart running pca on 2k feature array...feat layer={}\n'.format(feat_layer_file_name))
+
+    model_save_dir = '/home/amanda/Documents/vgg_model/2k_feat'
+    feat_layer_name = feat_layer_file_name[:-4]
+    train_test_split_data_save_name = os.path.join(model_save_dir, feat_layer_name+'_train_test_data.npz')
+
+    data = np.load(train_test_split_data_save_name)  # the data is generated by my_train_test_split func.
+    x_train, x_test, y_train, y_test = data['x_train'], data['x_test'], data['y_train'], data['y_test']
+
+    # apply PCA on training data.
+    pca = PCA(n_components=pca_keep_dim)
+    pca.fit(x_train)
+
+    cum_ratio = np.cumsum(pca.explained_variance_ratio_)[-1]
+    print('number of pc: {},  explained variance = {}\n'.
+          format(pca_keep_dim, cum_ratio))  # 0.98
+
+    # save the pca model
+    pca_save_name = '{}/{}_keep_{}_ratio_{}_pca_model.pkl'.format(
+        model_save_dir, feat_layer_name, str(pca_keep_dim), str(cum_ratio)[2:5])
+
+    print('Saving pca model...\n')
+
+    pickle.dump(pca, open(pca_save_name, 'wb'))
+
+    # # # when loading the model: loaded_model = pickle.load(open(file_name), 'wb')
+    # file_name = '/home/amanda/Documents/vgg_model/2k_feat/conv51_keep_num_200_keep_ratio_98_pca_model.pkl'
+    # with open(file_name, 'rb') as f:
+    #     pca = pickle.load(f)
+
+    # apply pca on raw feature matrix
+    x_train_pca = pca.transform(x_train)
+    x_test_pca = pca.transform(x_test)
+
+    # multi_regressor = MultiOutputRegressor(Ridge())
+    multi_regressor = MultiOutputRegressor(LinearRegression())
+    multi_regressor.fit(x_train_pca, y_train)
+
+    # save the regressor model
+    regressor_save_name = '{}/{}_keep_{}_regressor_model.pkl'.format(model_save_dir, feat_layer_name, str(pca_keep_dim))
+    # regressor_save_name = model_save_dir + '/' + feat_layer_name + '_regressor_model.pkl'
+    pickle.dump(multi_regressor, open(regressor_save_name, 'wb'))
+
+    # predict on training data
+    y_train_predict = multi_regressor.predict(x_train_pca)
+
+    # predict on test data
+    y_test_predict = multi_regressor.predict(x_test_pca)
+
+    # compare performance
+    rating_names = ['trustworthy', 'intelligent', 'attractive', 'aggressive', 'responsible', 'sociable']
+    for ind, cur_name in enumerate(rating_names):
+        print('Cur impression: {}'.format(cur_name))
+        train_gt = y_train[:, ind]
+        train_pred = y_train_predict[:, ind]
+        [train_cor, p] = pearsonr(train_gt, train_pred)
+
+        test_gt = y_test[:, ind]
+        test_pred = y_test_predict[:, ind]
+        [test_cor, p] = pearsonr(test_gt, test_pred)
+
+        print('train cor {}, test cor = {}\n'.format(train_cor, test_cor))
 
     return
 
 
-def feat_dic2matrix():
+def predict_on_e(feat_layer_file_name='conv53.npy'):
+    start_t = time.time()
+    # load extracted feature from feature_save_dir. Feature extraction layers include conv51,2,3, maxpool5, fc6, fc7.
+    feature_save_dir = '/home/amanda/Documents/vgg_model/e_with_mask_feat'
+    feature_file_name = os.path.join(feature_save_dir, feat_layer_file_name)
+    feat_arr = np.load(feature_file_name)  # xxxx * 100352, for example.
+    print('feature array shape = {}'.format(feat_arr.shape))
+
+    # Load pretrained model (pca model and the corresponding regressor)
+    model_load_dir = '/home/amanda/Documents/vgg_model/2k_feat'
+    pca_model_name = 'conv53_keep_150_ratio_986_pca_model.pkl'
+    regressor_model_name = 'conv53_keep_150_regressor_model.pkl'
+
+    file_name = os.path.join(model_load_dir, pca_model_name)
+    with open(file_name, 'rb') as f:
+        pca = pickle.load(f)
+
+    x_pca = pca.transform(feat_arr)
+
+    file_name = os.path.join(model_load_dir, regressor_model_name)
+    with open(file_name, 'rb') as f:
+        regressor = pickle.load(f)
+
+    y_predict = regressor.predict(x_pca)
+    np.save(os.path.join(feature_save_dir, 'conv51_prediction.npy'), y_predict)
+    print('Prediction saved.')
+    print('Done! Elapsed time = {}'.format(time.time() - start_t))
+
+
+def analysis_on_e_prediction():
+    e_prediction_dir = '/home/amanda/Documents/vgg_model/e_with_mask_feat'
+
+    impression_prediction_file_name = 'conv53_prediction.npy'
+    impression_prediction_file_path = os.path.join(e_prediction_dir, impression_prediction_file_name)
+    impression_array = np.load(impression_prediction_file_path)
+
+    valid_im_lst_file_name = 'valid_img_lst.npy'
+    valid_im_file_path = os.path.join(e_prediction_dir, valid_im_lst_file_name)
+    valid_im_lst = np.load(valid_im_file_path)
+
+    im_lst = pd.Series(valid_im_lst)
+
+    df = pd.DataFrame()
+    df['img_name'] = im_lst
+
+    rating_names = ['trustworthy', 'intelligent', 'attractive', 'aggressive', 'responsible', 'sociable']
+    img_save_root_dir = '/home/amanda/Documents/cropped_face'
+    img_src_dir = '/home/amanda/Documents/cropped_face/e_with_mask'
+    for ind, impression_name in enumerate(rating_names):
+        df[impression_name] = impression_array[:, ind]
+        attribute_dir = os.path.join(img_save_root_dir, impression_name)
+        if not os.path.exists(attribute_dir):
+            os.makedirs(attribute_dir)
+
+    df_len = len(df)
+    for ind, row in df.iterrows():
+        cur_img_name = row['img_name']
+        src_path = os.path.join(img_src_dir, cur_img_name)
+        if ind % 100 == 0:
+            print('{} out of {}'.format(ind+1, df_len))
+
+        for cur_attribute in rating_names:
+            cur_rating = row[cur_attribute]
+            dst_path = os.path.join(img_save_root_dir, cur_attribute, str(cur_rating)+'.jpg')
+            copyfile(src_path, dst_path)
 
     return
 
+
+# extract_e_feature()
 # extract_2kface_feature()
-split_on_2k()
+# split_on_2k()
+# pca_and_regression_on_2k()
+# predict_on_e()
+analysis_on_e_prediction()
+
+
 
