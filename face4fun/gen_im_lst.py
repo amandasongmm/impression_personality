ffb46f702a64506c7a0faf44775836d2349ea763
amandasongmm <feijuejuanling@gmail.com>
2018-05-16 13:51:09 -0700

    amt-exp-debug

---
+++ face4fun/gen_im_lst.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import random
+pd.options.mode.chained_assignment = None  # default='warn'
+
+# trustworthy_gen.csv: [less_trustworthy, more_trustworthy]
+# trustworthy_groundtruth.csv: [trustworthy_max, trustworthy_min]
+# aggressive_gen.csv: [less_aggressive, more_aggressive]
+# aggressive_groundtruth.csv: [aggressive_max, aggressive_min]
+
+
+# generate list for trustworthy only.
+
+
+def gen_attr_lst(attr='trustworthy'):
+    git_folder = 'https://raw.githubusercontent.com/amandasongmm/impression_personality/master/static/amt_'+attr + '/'
+    gen_img_lst = '../static/csv/trustworthy_gen.csv'
+    real_img_lst = '../static/csv/trustworthy_groundtruth.csv'
+
+    gen_im_df = pd.read_csv(gen_img_lst)
+    real_im_df = pd.read_csv(real_img_lst)
+
+    gen_im_df.columns = ['im1_name', 'im2_name', 'Task type']
+    gen_im_df['im1'] = '0'
+    gen_im_df['im2'] = '1'
+    gen_im_df['pair_ind'] = gen_im_df.index
+
+    random.seed(3)
+    rep_total = 20
+    gen_orig_len = len(gen_im_df)
+    gen_new_len = gen_orig_len + rep_total
+    rep_lst = random.sample(range(0, len(gen_im_df)), rep_total)
+
+    for count, ind in enumerate(rep_lst):
+        gen_im_df.loc[gen_orig_len + count] = gen_im_df.iloc[ind].values
+
+    gen_im_df['rep'] = 0
+    gen_im_df['rep'].iloc[gen_orig_len:gen_new_len] = 1
+
+    real_im_df.columns = ['more', 'less']
+    # change the order of the columns.
+    real_im_df = real_im_df[['less', 'more']]
+    real_im_df.columns = ['im1_name', 'im2_name']
+
+    # add the same additional labels.
+    real_im_df['Task type'] = 'ground_truth'
+    real_im_df['im1'] = '0'
+    real_im_df['im2'] = '1'
+    real_im_df['pair_ind'] = real_im_df.index + gen_orig_len
+    real_im_df['rep'] = 0
+
+    # merge two data frames
+    merge_df = real_im_df.append(gen_im_df, ignore_index=True)
+    rand_lst = [random.randint(0, 1) for i in range(0, len(merge_df))]
+
+    # change the left-right order of the pair.
+    merge_df.is_copy = False
+    for index, row in merge_df.iterrows():
+        im1_name = row['im1_name']
+        im2_name = row['im2_name']
+
+        if rand_lst[index] == 0:
+            merge_df['im1'].iloc[index] = '1'
+            merge_df['im2'].iloc[index] = '0'
+            merge_df['im1_name'].iloc[index] = git_folder+im2_name
+            merge_df['im2_name'].iloc[index] = git_folder+im1_name
+        else:
+            merge_df['im1_name'].iloc[index] = git_folder+im1_name
+            merge_df['im2_name'].iloc[index] = git_folder+im2_name
+
+    # shuffle the row orders.
+    new_merge_df = merge_df.sample(frac=1, random_state=1)
+
+    # write the df into txt format. list of lists.
+    test_lst = []
+    for ind, row in new_merge_df.iterrows():
+        test_lst.append(row.values.tolist())
+
+    txt_save_name = attr + '_shuffled_img_lst.txt'
+    csv_save_name = attr + '_shuffled_img_lst.csv'
+    save_dir = '../static/csv/'
+
+    with open(save_dir + txt_save_name, 'w') as file_handler:
+        for item in test_lst:
+            file_handler.write("{},\n".format(item))
+
+    new_merge_df.to_csv(save_dir+csv_save_name, index=False)
+
+
+gen_attr_lst()
