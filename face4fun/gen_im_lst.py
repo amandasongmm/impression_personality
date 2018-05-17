58a786dc106cd29162ac45131c2fae5dfee3148c
amandasongmm <feijuejuanling@gmail.com>
2018-05-17 00:42:59 -0700

    responsible local debug passed

--- face4fun/gen_im_lst.py
+++ face4fun/gen_im_lst.py
@@ -101,7 +101,78 @@ def gen_attr_lst(attr, stargan):
     new_merge_df.to_csv(save_dir+csv_save_name, index=False)
 
 
+def gen_from_one_df(attr):
+
+    im_pub_dir_prefix = 'https://raw.githubusercontent.com/amandasongmm/impression_personality/master/static/amt_'
+
+    im_pub_dir = im_pub_dir_prefix + attr + '/'
+
+    real_img_lst = '../static/csv/'+attr+'_ground_truth.csv'
+
+    real_im_df = pd.read_csv(real_img_lst)  # [min, max]
+    real_im_df.columns = ['im1_name', 'im2_name']
+    real_im_df['Task type'] = 'ground_truth'
+    real_im_df['im1'] = '0'
+    real_im_df['im2'] = '1'
+    real_im_df['pair_ind'] = real_im_df.index
+    real_im_df['rep'] = 0
+
+    random.seed(3)
+    rep_total = 20
+    orig_len = len(real_im_df)
+    new_len = orig_len + rep_total
+
+    # add some repetition to the generated images.
+    rep_lst = random.sample(range(0, len(real_im_df)), rep_total)
+
+    for count, ind in enumerate(rep_lst):
+        real_im_df.loc[orig_len + count] = real_im_df.iloc[ind].values
+
+    real_im_df['rep'].iloc[orig_len:new_len] = 1
+
+    # the original order of the real_im_df.
+
+    rand_lst = [random.randint(0, 1) for i in range(0, len(real_im_df))]
+
+    # change the left-right order of the pair.
+    real_im_df.is_copy = False
+    for index, row in real_im_df.iterrows():
+        im1_name = row['im1_name']
+        im2_name = row['im2_name']
+
+        if rand_lst[index] == 0:
+            real_im_df['im1'].iloc[index] = '1'
+            real_im_df['im2'].iloc[index] = '0'
+            real_im_df['im1_name'].iloc[index] = im_pub_dir+im2_name
+            real_im_df['im2_name'].iloc[index] = im_pub_dir+im1_name
+        else:
+            real_im_df['im1_name'].iloc[index] = im_pub_dir+im1_name
+            real_im_df['im2_name'].iloc[index] = im_pub_dir+im2_name
+
+    # shuffle the row orders.
+    new_real_im_df = real_im_df.sample(frac=1, random_state=1)
+
+    # write the df into txt format. list of lists.
+    test_lst = []
+    for ind, row in new_real_im_df.iterrows():
+        test_lst.append(row.values.tolist())
+
+    txt_save_name = attr + '_shuffled_img_lst.txt'
+    csv_save_name = attr + '_shuffled_img_lst.csv'
+
+    save_dir = '../static/csv/'
+
+    with open(save_dir + txt_save_name, 'w') as file_handler:
+        for item in test_lst:
+            file_handler.write("{},\n".format(item))
+
+    new_real_im_df.to_csv(save_dir+csv_save_name, index=False)
+    return
+
 # gen_attr_lst(attr='trustworthy', stargan=0)
 # gen_attr_lst(attr='aggressive', stargan=0)
 # gen_attr_lst(attr='trustworthy', stargan=1)
 # gen_attr_lst(attr='aggressive', stargan=1)
+
+
+gen_from_one_df('responsible')
