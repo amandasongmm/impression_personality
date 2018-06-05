bd038a3c22aeb062f2dd7f039e3517c8030c6c22
amandasongmm <feijuejuanling@gmail.com>
2018-06-05 15:39:53 -0700

    pipeline update

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -14,86 +14,16 @@ from os import listdir
 from os.path import isfile, join
 
 
-class CleanOldData:
-    def __init__(self,
-                 im_df_path='../pretrain_first_impression/tmp_data/vc_df.pkl',
-                 root_dir='/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/',
-                 crop_dir='/home/amanda/Documents/cropped_faces/vc/',
-                 crop_df_path='../pretrain_first_impression/tmp_data/vc_cropped_df.pkl'):
-        # im_df is prepared by crop_and_prepare_im_lst.py in Cleaning dir. (prepare_im_df)
-        self.im_df_path = im_df_path
-        self.root_dir = root_dir
-        self.crop_dir = crop_dir
-        self.crop_df_path = crop_df_path
-        self.face_cascade = cv2.CascadeClassifier('../ Cleaning/haarcascade_frontalface_default.xml')
-        # load haar cascade filters
-
-    def crop_face(self):
-        df = pd.read_pickle(self.im_df_path)
-        df['tw_exist'] = 0
-        df['cb_exist'] = 0
-
-        if not os.path.exists(self.crop_dir):
-            os.makedirs(self.crop_dir)
-
-        df_len = len(df)
-
-        for ind, row in df.iterrows():
-            if ind % 100 == 0:
-                print('{} out of {} people...'.format(ind, df_len))
-
-            tw_img_path = self.root_dir + row['tw_img_path']
-            cb_img_path = self.root_dir + row['cb_img_path']
-
-            # df.iloc[ind]['tw_img_path'] = ''
-            # df.iloc[ind]['cb_img_path'] = ''
-
-            # Deal with twitter data.
-            if os.path.isfile(tw_img_path):
-               img = cv2.imread(tw_img_path)
-               if img is not None:
-                   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                   faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-                   if len(faces) == 1:
-                       for (x, y, w, h) in faces:
-                           if w * h > 100:
-                               roi_color = img[int(y - 0.1 * h):int(y + 1.1 * h), int(x - 0.1 * w):int(x + 1.1 * w), :]
-                               img_save_name = self.crop_dir + row['short_id'] + '_tw.jpg'
-                               df.iloc[ind]['tw_img_path'] = img_save_name
-                               df.iloc[ind]['tw_exist'] = 1
-                               cv2.imwrite(img_save_name, roi_color)
-
-            # Deal with crunchbase data.
-            if os.path.isfile(cb_img_path):
-               img = cv2.imread(cb_img_path)
-               if img is not None:
-                   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                   faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-                   if len(faces) == 1:
-                       for (x, y, w, h) in faces:
-                           if w * h > 100:
-                               roi_color = img[int(y - 0.1 * h):int(y + 1.1 * h), int(x - 0.1 * w):int(x + 1.1 * w), :]
-                               img_save_name = self.crop_dir + row['short_id'] + '_cb.jpg'
-                               df.iloc[ind]['cb_img_path'] = img_save_name
-                               df.iloc[ind]['cb_exist'] = 1
-                               cv2.imwrite(img_save_name, roi_color)
-
-        df = df[df['tw_exist']+df['cb_exist'] != 0]
-        print('Done. {} people have at least one photo. Total photo number = {}'.
-              format(len(df), sum(df['cb_exist']+df['tw_exist'])))
-        df.to_pickle(self.crop_df_path)
-
-
 def fetch_file(path_to_folder, flag, key_word):
     '''
-    fetchFiles() requires three arguments: pathToFolder, flag and keyWord
+    fetch_files() requires three arguments: pathToFolder, flag and keyWord
 
     flag must be 'start_with' or 'end_with'
     keyWord is a string to search the file's name
 
     Be careful, the keyWord is case sensitive and must be exact
 
-    Example: fetchFiles('/Documents/Photos/','ENDS_WITH','.jpg')
+    Example: fetch_file('/Documents/Photos/','end_with','.jpg')
 
     returns: _pathToFiles and _fileNames
     '''
@@ -105,6 +35,7 @@ def fetch_file(path_to_folder, flag, key_word):
         if flag == 'end_with':
             raw_file = [item for item in file_names if item.endswith(key_word)]
             selected_file = [item[2:] if item.startswith('._') else item for item in raw_file]
+            selected_file = list(set(selected_file))
             _file_names.extend(selected_file)
 
             selected_path = [os.path.join(dir_path, item) for item in selected_file]
@@ -113,6 +44,7 @@ def fetch_file(path_to_folder, flag, key_word):
         elif flag == 'start_with':
             raw_file = [item for item in file_names if item.startswith(key_word)]
             selected_file = [item[2:] if item.startswith('._') else item for item in raw_file]
+            selected_file = list(set(selected_file))
             _file_names.extend(selected_file)
 
             selected_path = [os.path.join(dir_path, item) for item in selected_file]
@@ -137,11 +69,28 @@ def fetch_file(path_to_folder, flag, key_word):
     return _path_to_files, _file_names
 
 
-def crop_faces(crop_dir='/home/amanda/Documents/cropped_faces/vc/',
-               crop_with_mask_dir='/home/amanda/Documents/cropped_faces/vc_with_mask/'):
-    path_to_files, file_names = fetch_file(
-        '/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/',
-        'end_with', 'jpeg')
+def crop_faces(
+        raw_data_dir='/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/',
+        crop_dir='/home/amanda/Documents/cropped_faces/vc/',
+        crop_with_mask_dir='/home/amanda/Documents/cropped_faces/vc_with_mask/'):
+
+    '''
+        crop_faces() requires three arguments: raw_data_dir, crop_dir and crop_with_mask_dir
+
+        raw_data_dir is the root dir that you want to run the analysis on. e.g.
+        '/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/' will search for all the *.jpg
+        files under the subfolder of vc recursively. namely, all the vc images.
+
+        crop_dir is a folder where you want to save the cropped faces (with no mask)
+
+        crop_with_mask_dir is where you want to save the cropped and masked faces.
+
+        Example: raw_data_dir='/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/',
+        crop_dir='/home/amanda/Documents/cropped_faces/vc/',
+        crop_with_mask_dir='/home/amanda/Documents/cropped_faces/vc_with_mask/'
+    '''
+
+    path_to_files, file_names = fetch_file(raw_data_dir, 'end_with', 'jpeg')  # get all the files in the sub directories
 
     total_file_len = len(file_names)
     if not os.path.exists(crop_dir):
@@ -193,6 +142,11 @@ def crop_faces(crop_dir='/home/amanda/Documents/cropped_faces/vc/',
     return
 
 
-# crop_faces()
+# change the raw_data_dir, crop_dir and crop_with_mask_dir to fit your need.
+raw_data_dir = ''
+crop_dir = ''
+crop_with_mask_dir = ''
+crop_faces(raw_data_dir, crop_dir, crop_with_mask_dir)
+
 # libpng warning: iCCP: known incorrect sRGB profile
 # libpng warning: iCCP: profile 'icc': 1000000h: invalid rendering intent
