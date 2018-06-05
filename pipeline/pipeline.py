570c4173b22fbc249f9bb6140427396800be276b
amandasongmm <feijuejuanling@gmail.com>
2018-06-05 01:00:52 -0700

    crop pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,6 +10,8 @@ import time
 import pandas as pd
 import pickle
 from scipy.stats.stats import pearsonr
+from os import listdir
+from os.path import isfile, join
 
 
 class CleanOldData:
@@ -132,13 +134,65 @@ def fetch_file(path_to_folder, flag, key_word):
             print('No files with given parameters were found in {}\n'.format(dir_path))
 
     print('{} files are found in searched folders.'.format(len(_file_names)))
-    a = 12
     return _path_to_files, _file_names
 
 
-# clean_pipe = CleanOldData()
-# clean_pipe.crop_face()
+def crop_faces(crop_dir='/home/amanda/Documents/cropped_faces/vc/',
+               crop_with_mask_dir='/home/amanda/Documents/cropped_faces/vc_with_mask/'):
+    path_to_files, file_names = fetch_file(
+        '/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/',
+        'end_with', 'jpeg')
 
-a, b = fetch_file('/home/amanda/Documents/VC and Entrepreneur Faces Project/Extracted Faces/vc/', 'end_with', 'jpeg')
+    total_file_len = len(file_names)
+    if not os.path.exists(crop_dir):
+        os.makedirs(crop_dir)
+
+    if not os.path.exists(crop_with_mask_dir):
+        os.makedirs(crop_with_mask_dir)
+
+    face_cascade = cv2.CascadeClassifier('../Cleaning/haarcascade_frontalface_default.xml')
+    count = 0
+    for path, file_name in zip(path_to_files, file_names):
+        img = cv2.imread(path)
+        if img is not None:
+            height, width, channel = img.shape
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            if len(faces) == 1:
+                for (x, y, w, h) in faces:
+                    if (w * h > 100 and int(y - 0.1 * h)>0 and int(y + 1.1 * h)<height and int(x - 0.1 * w)>0 and int(x + 1.1 * w)<width):
+                        count += 1
+                        roi_color = img[int(y - 0.1 * h):int(y + 1.1 * h), int(x - 0.1 * w):int(x + 1.1 * w), :]
+                        img_save_name = crop_dir + file_name
+                        cv2.imwrite(img_save_name, roi_color)
+
+                        # add mask and save masked image.
+                        mask = np.zeros_like(roi_color)
+                        rows, cols, _ = mask.shape
+
+                        # create a black filled ellipse
+                        cen_x = int(rows // 2)
+                        cen_y = int(cols // 2)
+                        cv2.ellipse(mask, center=(cen_y, cen_x), axes=(cen_y, cen_x), angle=0.0, startAngle=0.0,
+                                    endAngle=360.0, color=(255, 255, 255), thickness=-1)
+                        # bitwise
+                        result = np.bitwise_and(roi_color, mask)
+
+                        # flipped mask
+                        new_mask = 255 * np.ones_like(roi_color)
+                        cv2.ellipse(new_mask, center=(cen_y, cen_x), axes=(cen_y, cen_x), angle=0.0, startAngle=0.0,
+                                    endAngle=360,
+                                    color=(0, 0, 0), thickness=-1)
+
+                        # final result
+                        with_mask = result + new_mask
+                        cv2.imwrite(crop_with_mask_dir + file_name, with_mask)
+
+                        if count % 100 == 0:
+                            print count, total_file_len
+    return
 
 
+# crop_faces()
+# libpng warning: iCCP: known incorrect sRGB profile
+# libpng warning: iCCP: profile 'icc': 1000000h: invalid rendering intent
