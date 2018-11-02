0f63c66735737919da11d012dc20beb4dfb142f3
ssomashekhar <30468383+SuprabhaSomashekhar@users.noreply.github.com>
2018-11-01 22:01:18 -0700

    Update faceplusapi--inprogress.py
    Adding changes

--- faceplusapi--inprogress.py
+++ faceplusapi--inprogress.py
@@ -5,15 +5,14 @@ import glob
 import time
 import csv
 
-file = open("ent-details.txt", "a") 
-file.write("image_name"+ "\t"+"ethnicity"+ "\t"+"gender"+"\t"+"age"+"\t"+"\n")
-for img_filename in glob.iglob('/Users/suprabhasomashekhar/Downloads/*.jpg'):
+file = open("ent-details.txt", "w") 
+file.write("image_name"+ "\t"+"ethnicity"+ "\t"+"gender"+"\t"+"age"+"\t"+"beauty-female"+"\t"+"beauty-male"+"\t"+"\n")
+for img_filename in glob.iglob('/Users/suprabhasomashekhar/Downloads/e/*/*.jpeg'):
 	with open(img_filename, 'rb') as f:
 		img_data = f.read()
-		print(img_filename)
 		http_url="https://api-us.faceplusplus.com/facepp/v3/detect"
 		path='/Users/suprabhasomashekhar/Downloads/'
-		data={ "api_key": "8hvaF5CZTdoyaIOmxGNWbcPZzCTKJNUS","api_secret":"Pas8ETS8wSzEAFOeGh_r8frqGE3rhwU5","return_landmark":0, "return_attributes":"ethnicity,gender,age"}
+		data={ "api_key": "8hvaF5CZTdoyaIOmxGNWbcPZzCTKJNUS","api_secret":"Pas8ETS8wSzEAFOeGh_r8frqGE3rhwU5","return_landmark":0, "return_attributes":"ethnicity,gender,age,beauty"}
 		files= {"image_file": open(img_filename,'rb')}
 		response=requests.post(http_url,data=data,files=files)
 		time.sleep(2)
@@ -21,7 +20,11 @@ for img_filename in glob.iglob('/Users/suprabhasomashekhar/Downloads/*.jpg'):
 		print(req_con)
 		index=img_filename.rfind("/")
 		img_name=img_filename[index+1:]
+		print(img_name)
 		resp_dict = json.loads(req_con)
-		file.write(img_name+ "\t"+str(resp_dict['faces'][0]["attributes"]["ethnicity"]["value"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["gender"]["value"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["age"]["value"])+ "\t"+"\n")
-		
-		
+		try:
+			file = open("ent-details.txt", "a") 
+			file.write(img_name+ "\t"+str(resp_dict['faces'][0]["attributes"]["ethnicity"]["value"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["gender"]["value"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["age"]["value"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["beauty"]["female_score"])+ "\t"+str(resp_dict['faces'][0]["attributes"]["beauty"]["male_score"])+ "\t"+"\n")
+		except:
+			continue
+file.close()
